97433f30adc4162529290eff0cac73235d77d2b6
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-14 13:42:35 -0300

    Create questao6.py

---
+++ questao6.py
@@ -0,0 +1,15 @@
+print('Tabuada de 1 a 10\n-----------------')
+num = int(input('Qual Tabuada Deseja Ver?: '))
+
+
+if 1 <= num <= 10:
+    tabuada = 0
+    print('__________________')
+    for x in range (1,10):
+        tabuada += 1
+        result = num * tabuada
+        print(f'{num} x {tabuada} = {result}')
+    print('__________________')
+
+else:
+    print('-------------------\nApenas Números de 1 a 10 Serão Mostrados')
