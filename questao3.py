503053b9f722f87cef3e15f74c78ba15d29be124
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-14 12:50:55 -0300

    Create questao3.py

---
+++ questao3.py
@@ -0,0 +1,7 @@
+print('Progressão Geométrica\n------------')
+a0 = int(input('Digite o início da Progressão: '))
+r = int(input('Digite a Razão Da Progressão: '))
+limite = int(input('Digite o Termo Limite da Progressão: '))
+
+for x in range(a0,limite+1, r):
+    print(x)
