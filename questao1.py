ff7f29a1c2423bdf582f19b54803615365e2b45b
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-14 12:47:07 -0300

    Create questao1.py

---
+++ questao1.py
@@ -0,0 +1,7 @@
+print('Números Inteiros de 1 até N\n-------------------')
+n = int(input('Até Qual Valor Deseja Mostrar os Números Inteiros: '))
+numeros = 0
+
+for x in range (0, n):
+    numeros += 1
+    print(numeros)
