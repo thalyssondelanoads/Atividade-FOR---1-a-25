bcbbc5634a6e263667c2f068de9954a0109b4863
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-13 19:21:58 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,8 +1,8 @@
 def main():
   print('Números Inteiros Pares de 1 até N\n-------------------')
   n = int(input('Até Qual Valor Deseja Mostrar os Números Inteiros Pares: '))
-  numeros = 0
   
+  numeros = 0
   for x in range (1, n):
       numeros += 1
       if x % 2 == 0:
