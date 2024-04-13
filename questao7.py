ad9afece49fefcabd509f97277133af7114984ca
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-13 19:36:11 -0300

    Update questao7.py

--- questao7.py
+++ questao7.py
@@ -1,9 +1,12 @@
-print('Soma Dos Números Entre 1 e N\n-----------------------')
-n = int(input('Até Qual Número Deseja Somar: '))
+def main():
+  print('Soma Dos Números Entre 1 e N\n-----------------------')
+  n = int(input('Até Qual Número Deseja Somar: '))
+  
+  numeros = 0
+  soma = 0
+  for x in range (1,n+1):
+      numeros += 1
+      soma += numeros
+  print(f'-------------------------\nA Soma de Todos os Números entre 1 e {n} = {soma}')
 
-numeros = 0
-soma = 0
-for x in range (1,n+1):
-    numeros += 1
-    soma += numeros
-print(f'-------------------------\nA Soma de Todos os Números entre 1 e {n} = {soma}')
+main()
