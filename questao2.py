306af1154b59cf382c782f90f304122e87fc7836
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-13 19:20:24 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,8 +1,11 @@
-print('Números Inteiros Pares de 1 até N\n-------------------')
-n = int(input('Até Qual Valor Deseja Mostrar os Números Inteiros Pares: '))
-numeros = 0
+def main():
+  print('Números Inteiros Pares de 1 até N\n-------------------')
+  n = int(input('Até Qual Valor Deseja Mostrar os Números Inteiros Pares: '))
+  numeros = 0
+  
+  for x in range (1, n):
+      numeros += 1
+      if x % 2 == 0:
+          print(x)
 
-for x in range (0, n):
-    numeros += 1
-    if numeros % 2 == 0:
-        print(numeros)
+main()
