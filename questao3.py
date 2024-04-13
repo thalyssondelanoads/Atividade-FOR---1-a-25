9d1d1671ecee9aaf325f226429cd6c88d1742341
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-13 19:21:43 -0300

    Update questao3.py

--- questao3.py
+++ questao3.py
@@ -1,7 +1,10 @@
-print('Progressão Aritmética\n------------')
-a0 = int(input('Digite o início da Progressão: '))
-r = int(input('Digite a Razão Da Progressão: '))
-limite = int(input('Digite o Termo Limite da Progressão: '))
+def main():
+  print('Progressão Aritmética\n------------')
+  a0 = int(input('Digite o início da Progressão: '))
+  r = int(input('Digite a Razão Da Progressão: '))
+  limite = int(input('Digite o Termo Limite da Progressão: '))
+  
+  for x in range(a0,limite+1, r):
+      print(x)
 
-for x in range(a0,limite+1, r):
-    print(x)
+main()
