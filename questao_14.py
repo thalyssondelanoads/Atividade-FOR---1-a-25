89ef666b6cac84da43745eaea1b74e021a921bc7
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-13 21:09:14 -0300

    Update questao_14.py

--- questao_14.py
+++ questao_14.py
@@ -1,1 +1,11 @@
+def main():
+  print('Maior Quadrado Igual ou Menor que N\n--------------------------')
+  numero = int(input('Digite o Número : '))
 
+  for x in range (numero,0,-1):
+    quadrado = x**0.5
+    if quadrado == int(quadrado):
+      print(f'_________________\nO Maior Quadrado é {x}')
+      break
+      
+main()
