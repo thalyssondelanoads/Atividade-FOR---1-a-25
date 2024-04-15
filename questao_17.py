8b62018158674f712f04870100170198df39ca8c
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-14 23:02:01 -0300

    Update questao_17.py

--- questao_17.py
+++ questao_17.py
@@ -2,7 +2,7 @@ def calcular_e_mostrar_soma(n):
   soma = 0
   for denominador in range(1,n+1):
     soma += 1/denominador                #FUNÇÃO QUE INCREMENTA OU DECREMENTA OS NÚMEROS DAS FRAÇÕES
-    print(f'1/{denominador}',end='')     #REALIZA A SOMA DAS MESMAS LOGO EM SEGUIDA
+    print(f'1/{denominador}',end='')     #REALIZA O CÁLCULO DAS MESMAS LOGO EM SEGUIDA
     print(' + ' if denominador < n else ' = ' , end='')
   
   return soma
