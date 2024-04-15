5aabd04ec903eea77b4b76f8ca0b385f01e35b91
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-15 10:58:12 -0300

    Update questao_19.py

--- questao_19.py
+++ questao_19.py
@@ -1,17 +1,1 @@
-def eh_primo(numero):
-    if numero <= 1:
-        return False
-    for i in range(2, numero):
-        if numero % i == 0:
-            return False
-    return True
 
-def main():
-    limite_inferior = int(input('Digite o Limite Inferior : '))
-    limite_superior = int(input('Digite o Limite Superior : '))
-
-    for numeros in range (limite_inferior,limite_superior+1):
-        if eh_primo(numeros):
-            print(numeros)
-
-main()
