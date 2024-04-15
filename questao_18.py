64c73f031c9b3d917ee4eaef294bf3651eefbd40
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-14 22:24:32 -0300

    Update questao_18.py

--- questao_18.py
+++ questao_18.py
@@ -1,1 +1,21 @@
+def calcular_e_mostrar_soma(n):
+  soma = 0
+  denominador = n
+  for x in range(1, n + 1):
+    soma += x / denominador
+    print(f'{x}/{denominador}',end='')
+    print(' + ' if x < n else ' = ' , end='')
+    denominador -= 1
 
+  return soma
+
+def main():
+  print('Soma de Frações, Ex : 1/N + 2/N-1 + 3/N-2\n------------------------------')
+  n = int(input('Digite o Número de Somas que Deseja : '))
+
+  print('-'*30)
+  soma = calcular_e_mostrar_soma(n)
+
+  print(f'{soma:.3f}')
+
+main()
