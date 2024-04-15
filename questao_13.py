335c2fed91457bbbc0c39a659e3e54c4d3771879
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-15 10:08:57 -0300

    Update questao_13.py

--- questao_13.py
+++ questao_13.py
@@ -1,1 +1,17 @@
+def main():
+    print('Maior Número da Lista\n-----------------------')
 
+    n = int(input('Quantos Números Deseja Digitar : '))
+
+    for x in range (1,n+1):
+        numero = float(input(f'Digite o {x}º Número : '))
+
+        if x == 1:
+            numero_maior = numero
+        else:
+            if numero > numero_maior:
+                numero_maior = numero
+        
+    print(f'-----------------------\nMaior Número : \033[36m{numero_maior}\033[m')
+            
+main()
