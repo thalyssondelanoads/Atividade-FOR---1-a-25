18bb7f43873ed3b615c1b61cc869e6f5a0903667
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-15 09:56:54 -0300

    Update questao5.py

--- questao5.py
+++ questao5.py
@@ -1,1 +1,14 @@
-.
+def main():
+    print('Fatorial de um Número\n-----------------------')
+
+    n = int(input('Digite o Número que Deseja Calcular: '))
+    
+    fatorial = 1
+    for sequencia in range(1,n+1):
+        fatorial *= sequencia
+        print(sequencia,end=' ')
+        print('x' if sequencia < n else '=',end=' ')
+    
+    print(fatorial)
+
+main()
