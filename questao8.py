855486bad1965085288a0e4574102cbd65278352
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-05-01 23:31:00 -0300

    Update questao8.py

--- questao8.py
+++ questao8.py
@@ -1,11 +1,11 @@
 def main():
   print("Múltiplos de N em uma Sequência")
-  N = int(input("Insira o valor de N: "))
+  n = int(input("Insira o valor de N: "))
   limite_inferior = int(input("Insira o Limite inferior: "))
   limite_superior = int(input("Insira o Limite superior: "))
   
   for x in range(limite_inferior,limite_superior+1):
-      if x % N == 0:
+      if x % n == 0:
           print(x)
 
 main()
