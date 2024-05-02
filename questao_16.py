1b167eefede3e6587eb7115809be2b21060f7b01
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-05-01 23:33:36 -0300

    Update questao_16.py

--- questao_16.py
+++ questao_16.py
@@ -1,1 +1,13 @@
+def main():
+  N = int(input("Insira o valor de N (Maior ou Igual a 2): "))
+  fib1, fib2 = 0, 1
 
+  print(fib1)
+  print(fib2)
+
+  for x in range(2, N):
+      fib_atual = fib1 + fib2
+      print(fib_atual)
+      fib1, fib2 = fib2, fib_atual
+  
+main()
