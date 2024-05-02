fda02de60cd194761d26c41551e40dce39130242
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-05-01 23:27:39 -0300

    Update questao4.py

--- questao4.py
+++ questao4.py
@@ -1,1 +1,12 @@
-.
+def main():
+  print("Progressão Geométrica\n-------------------")
+  A0 = int(input("Valor inicial : "))
+  Limite = int(input("Insira o Limite : "))
+  R = int(input("Razão : "))
+  
+  atual = A0
+  while atual < Limite:
+      print(atual)
+      atual *= R
+
+main()
