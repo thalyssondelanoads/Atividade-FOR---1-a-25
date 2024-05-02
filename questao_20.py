bf3239e0747723373dc0caa41c21f2ea76fa5c1d
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-05-01 23:44:01 -0300

    Update questao_20.py

--- questao_20.py
+++ questao_20.py
@@ -1,1 +1,21 @@
+def main():
+  N = int(input("Insira o valor de N: "))
 
+  total = 0
+  sequencia = ""
+  for i in range(1, N + 1):
+      if i % 2 == 0:
+          total -= 1 / i
+          sequencia += f"- 1/{i}"
+      else:
+          if i == 1:
+              sequencia += f"1/{i}"
+          else:
+              sequencia += f"+ 1/{i}"
+
+      if i != N:
+          sequencia += " "
+
+  print(sequencia, "=", total)
+
+main()
